0ece04ea6ccf8f5731147c854882d7a63a7bd698
yuliuftd <liuyuftd@gmail.com>
2021-04-06 11:29:48 +1000

    cfront end side and redux

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return (
-        <section className="landing">
+    <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="x-large">Developer Connector</h1>
@@ -11,8 +12,8 @@ const Landing = () => {
             other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+            <Link to="/login" className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
