abc94a759c648021152bfb57204b855ec7fdb28b
yuliuftd <liuyuftd@gmail.com>
2021-04-06 13:13:40 +1000

    setAlert component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,11 @@
 import React , { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 // import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-
-const Register = () => {
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({ 
         name: '',
         email: '',
@@ -18,29 +20,11 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2){
-            console.log('password do no match')
+            setAlert('password do no match', 'danger')
         }else {
-            console.log('Success!')
-            // const newUser = {
-            //     name,
-            //     email,
-            //     password
-            // }
-            // try {
-            //     const config = {
-            //         headers: {
-            //             'Content-Type': 'application/json'
-            //         }
-            //     }
-            //     const body = JSON.stringify(newUser);
-            //     const res = await axios.post('/api/users', body, config);
-            //     console.log(res.data);
-            // } catch (err) {
-            //     console.error(err.response.data);
-                
-            // }
+            console.log('Success!');
         }
-    }
+    };
     return (
     <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -82,7 +66,11 @@ const Register = () => {
         Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
-    )
+    );
+};
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
 }
 
-export default Register
+export default connect(null, { setAlert })(Register);
