7d0252e8cebc2cb2b636260dee1ce0d39d816d39
yuliuftd <liuyuftd@gmail.com>
2021-04-07 12:08:59 +1000

    Login and logout function done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
-const App = () => (
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = () => {
+  useEffect(()=>{
+    store.dispatch(loadUser());
+  }, []);
+  
+  return (
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -22,10 +35,12 @@ const App = () => (
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
+            <Route exact path='/dashboard' component={Dashboard} />
           </Switch>
         </section>
       </Fragment>
     </Router>
   </Provider>
 );
+}
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React , { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 // import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({ 
         name: '',
         email: '',
@@ -22,9 +23,13 @@ const Register = ({ setAlert }) => {
         if (password !== password2){
             setAlert('password do no match', 'danger')
         }else {
-            console.log('Success!');
+            register({ name, email, password });
         }
     };
+
+    if(isAuthenticated) {
+      return <Redirect to='/dashboard' />
+    }
     return (
     <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -70,7 +75,13 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
-}
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
